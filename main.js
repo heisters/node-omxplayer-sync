c630079b772048b7137189cd2a939a98e87bb012
Ian Heisters <ian@heisters.co>
2015-08-11 15:04:30 -0700

    Attempt to connect to dbus multiple times before failing

--- main.js
+++ main.js
@@ -64,9 +64,41 @@ function handlePacket (packet) {
 
 ////////////////////////////////////////////////////////////////////////////////
 // DBus omxplayer control
-var bus = dbus.sessionBus({
-  busAddress: fs.readFileSync('/tmp/omxplayerdbus.'+process.env.USER, 'ascii').trim()
-});
+function Bus() {
+}
+
+Bus.prototype = EventEmitter.prototype;
+
+Object.defineProperties( Bus.prototype, {
+  invoke: { value: function() {
+    this.dbus.invoke.apply( this.dbus, arguments );
+  } },
+
+  create: { value: function( tries ) {
+    tries = tries === undefined ? 3 : tries;
+    tries--;
+
+    try {
+      this.dbus = dbus.sessionBus({
+        busAddress: fs.readFileSync('/tmp/omxplayerdbus.'+process.env.USER, 'ascii').trim()
+      });
+    } catch ( e ) {
+      if ( e.code === "ENOENT" && tries >= 0 ) {
+        console.log( "Could not connect to dbus, trying again." );
+        setTimeout( function() { this.create( tries ) }.bind( this ), 500 );
+        return;
+      } else {
+        console.log( "Failed to connect to dbus." );
+        throw e;
+      }
+    }
+
+    this.emit( "ready", this.dbus );
+
+  } }
+} );
+
+var bus = new Bus();
 
 var INTERFACE_SHORT_TO_FULL = {
   properties: 'org.freedesktop.DBus.Properties',
@@ -78,6 +110,7 @@ function PlayerController( bus, clock ) {
   this.sync = { seconds: -1, time: -1, invalid: true };
   this.speed = 0;
   this.clock = clock;
+  this.waiting = false;
 }
 
 Object.defineProperties( PlayerController.prototype, {
@@ -165,68 +198,71 @@ Object.defineProperties( PlayerController.prototype, {
 
   time: {
     get: function() { return this.sync.time; }
-  }
+  },
+
+  synchronize: { value: function( seconds, time ) {
+    if ( this.waiting || ! this.isReadyForSync() || seconds < 0 ) return;
+
+    if ( ! this.clock.isSynchronized ) console.log( "synchronizing clock to master time" );
+    this.clock.sync( time ); // doesn't compensate for latency...
+
+    var now             = this.clock.now()
+      , masterPosition  = seconds + ( now - time ) / 1e3
+      , localPosition   = this.seconds + ( now - this.time ) / 1e3
+      , delta           = localPosition - masterPosition
+      , absDelta        = Math.abs( delta );
+
+    if ( absDelta > TOLERANCE ) {
+      this.invalid = true;
+
+      if ( absDelta < FINE_TUNE_TOLERANCE ) {
+        console.log( "sync fine-tune", delta );
+
+        if ( delta > 0 ) this.slower();
+        else this.faster();
+
+      } else {
+        console.log( "sync jump", delta );
+
+        if ( delta > 0 ) {
+          this.waiting = true;
+          this.pause( function( err ) {
+            if ( !err ) {
+              var waitFor = delta * 1e3 - ( this.clock.now() - now );
+              setTimeout( function() {
+                this.play( function(){} );
+                this.waiting = false;
+              }, waitFor );
+            } else this.waiting = true;
+          }.bind( this ) );
+
+        } else {
+          this.setPosition( masterPosition );
+        }
+      }
+
+    } else {
+      // ensure speed is reset
+      while( this.speed < 0 ) this.faster();
+      while( this.speed > 0 ) this.slower();
+    }
+  } }
 
 } );
 
 var controller = new PlayerController( bus, clock );
-controller.pollStatus();
+
+bus.on( "ready", function( dbus ) {
+  controller.pollStatus();
+} );
 
 ////////////////////////////////////////////////////////////////////////////////
 // Cluster
 
-var waiting = false;
-oscRouter.on( "/sync", function( args ) {
-  var master = { seconds: args[0], time: args[1].native };
-  if ( waiting || ! controller.isReadyForSync() || master.seconds < 0 ) return;
-
-  if ( !clock.isSynchronized ) console.log( "synchronizing clock to master time" );
-  clock.sync( master.time ); // doesn't compensate for latency...
-
-  var now = clock.now();
-  var masterPosition = master.seconds + ( now - master.time ) / 1e3;
-  var localPosition = controller.seconds + ( now - controller.time ) / 1e3;
-  var delta = localPosition - masterPosition;
-  var absDelta = Math.abs( delta );
-
-  if ( absDelta > TOLERANCE ) {
-    controller.invalid = true;
-
-    if ( absDelta < FINE_TUNE_TOLERANCE ) {
-      console.log( "sync fine-tune", delta );
-
-      if ( delta > 0 ) {
-        controller.slower();
-      } else {
-        controller.faster();
-      }
-
-    } else {
-      console.log( "sync jump", delta );
-
-      if ( delta > 0 ) {
-        waiting = true;
-        controller.pause( function( err ) {
-          if ( !err ) {
-            var waitFor = delta * 1e3 - (clock.now() - now);
-            setTimeout( function() {
-              controller.play( function(){} );
-              waiting = false;
-            }, waitFor );
-          } else waiting = true;
-        } );
-
-      } else {
-        controller.setPosition( masterPosition );
-      }
-    }
-
-  } else {
-    // ensure speed is reset
-    while( controller.speed < 0 ) { controller.faster(); }
-    while( controller.speed > 0 ) { controller.slower(); }
-  }
-
+bus.on( "ready", function() {
+  oscRouter.on( "/sync", function( args ) {
+    controller.synchronize( args[ 0 ], args[ 1 ].native );
+  } );
 } );
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -251,3 +287,4 @@ var args = [];
 //args = args.concat(["--win", "0,0,960,540"]);
 omx.play( filename, {loop: true, args: args} );
 
+bus.create();
